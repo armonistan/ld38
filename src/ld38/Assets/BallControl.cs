55a1544aaf24998f284c149e7033022ab71586e7
dpumford <dpumford@hotmail.com>
2017-04-22 09:17:43 -0500

    Adds basic ball behavior.

---
+++ src/ld38/Assets/BallControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallControl : MonoBehaviour
+{
+    public float Speed;
+    public float DegAngle;
+
+    public float RadAngle
+    {
+        get { return Mathf.Deg2Rad * DegAngle; }
+    }
+
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    this.gameObject.transform.Translate(new Vector3(Mathf.Cos(RadAngle), Mathf.Sin(RadAngle)) * Speed * Time.deltaTime);
+	}
+}
