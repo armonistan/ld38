e7c3c8bc76b41b453cea2acc9e0b848fbdd72157
Riley Turben <Riley.Turben@walmart.com>
2017-04-23 16:10:32 -0500

    Setting up UI in game scene

--- src/ld38/Assets/ScoreManager.cs
+++ src/ld38/Assets/ScoreManager.cs
@@ -5,21 +5,36 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour {
 	public Text Score;
+	public Text AddedScore;
 
 	private int _score;
-	private string SCORE_BASE_STRING = "Score: ";
+	private int _multiplier;
 
 	// Use this for initialization
 	void Start () {
 		_score = 0;
+		_multiplier = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Score.text = SCORE_BASE_STRING + _score;
+		Score.text = _score.ToString();
 	}
 
-	public void UpdateScore(int scoreToAdd){
-		_score += scoreToAdd;
+	public void AddScore(int scoreToAdd) {
+		_score += scoreToAdd * _multiplier;
+		AddedScore.text = "+" + scoreToAdd.ToString();
+		if (_multiplier > 1) 
+		{
+			AddedScore.text = AddedScore.text + "x" + _multiplier.ToString() + "!";
+		}
+	}
+
+	public void BoostMultiplier() {
+		_multiplier += 1;
+	}
+
+	public void ResetMultipler() {
+		_multiplier = 1;
 	}
 }
