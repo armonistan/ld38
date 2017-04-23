44553f75b121bacf9cc79a620719c48ca3894cab
dpumford <dpumford@hotmail.com>
2017-04-23 14:13:48 -0500

    Adds faster and slower powerups.

--- src/ld38/Assets/Scripts/BallControl.cs
+++ src/ld38/Assets/Scripts/BallControl.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Assets.Scripts;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -14,17 +15,14 @@ public class BallControl : StatefulMonoBehavior<BallControl.States>
     }
 
 	public float DegAngle;
-	public float StrongReflectMultiplier = 2;
-    public float Speed;
-	public float MaxSpeed = 4;
+    public float Speed = 4;
+    public float[] SpeedClasses;
+    public int CurrentSpeedClass = 0;
 	public float BallSteeringMagnitude = 1;
     
     public int PauseFrames = 5;
 
     public ObstacleControl.PowerupType ActivePowerup;
-    public int PowerupFrames;
-
-    private int _powerupCounter;
 
     public float RadAngle
     {
@@ -39,19 +37,11 @@ public class BallControl : StatefulMonoBehavior<BallControl.States>
     {
         get
         {
-			if (Input.GetKey (LeftBallSteering)) {
-				RadAngle += (BallSteeringMagnitude* Time.deltaTime);
-			} else if (Input.GetKey (RightBallSteering)) {
-				RadAngle -= (BallSteeringMagnitude* Time.deltaTime);
-			}
-			return new Vector2 (Mathf.Cos (RadAngle), Mathf.Sin (RadAngle)) * Speed * Time.deltaTime;
+            Speed = Mathf.Clamp(SpeedClasses[CurrentSpeedClass], SpeedClasses.First(), SpeedClasses.Last());
+            return new Vector2 (Mathf.Cos (RadAngle), Mathf.Sin (RadAngle)) * Speed * Time.deltaTime;
 		}
         set
         {
-            Speed = value.magnitude / Time.deltaTime;
-			if (Speed > MaxSpeed) {
-				Speed = MaxSpeed;
-			}
             RadAngle = Mathf.Atan2(value.y, value.x);
         }
     }
@@ -68,15 +58,45 @@ public class BallControl : StatefulMonoBehavior<BallControl.States>
 	        return;
 	    }
 
+	    if (Input.GetKey(LeftBallSteering))
+	    {
+	        RadAngle += (BallSteeringMagnitude * Time.deltaTime);
+	    }
+	    else if (Input.GetKey(RightBallSteering))
+	    {
+	        RadAngle -= (BallSteeringMagnitude * Time.deltaTime);
+	    }
+
+	    switch (ActivePowerup)
+	    {
+	        case ObstacleControl.PowerupType.None:
+	            GetComponent<Renderer>().material.color = Color.white;
+                break;
+	        case ObstacleControl.PowerupType.Multiball:
+	            GetComponent<Renderer>().material.color = Color.magenta;
+                break;
+	        case ObstacleControl.PowerupType.Slower:
+	            GetComponent<Renderer>().material.color = Color.blue;
+                break;
+	        case ObstacleControl.PowerupType.Faster:
+	            GetComponent<Renderer>().material.color = Color.red;
+                break;
+	        case ObstacleControl.PowerupType.Shield:
+	            GetComponent<Renderer>().material.color = Color.green;
+                break;
+	        case ObstacleControl.PowerupType.Pointmania:
+	            GetComponent<Renderer>().material.color = Color.yellow;
+                break;
+	        default:
+	            throw new ArgumentOutOfRangeException();
+	    }
+
         switch (State)
 	    {
 		case States.Idle:
-				GetComponent<Renderer> ().material.color = Color.white;
 				gameObject.transform.Translate(Velocity);
 	            break;
 	        case States.Pause:
-	            GetComponent<Renderer>().material.color = Color.yellow;
-
                 if (Counter > PauseFrames)
 	            {
 	                State = States.GameOver;
@@ -87,8 +107,6 @@ public class BallControl : StatefulMonoBehavior<BallControl.States>
 	            }
 	            break;
 			case States.Bounce:
-				GetComponent<Renderer> ().material.color = Color.red;
-
 				gameObject.transform.Translate(Velocity);
                 break;
 	        case States.GameOver:
@@ -96,16 +114,6 @@ public class BallControl : StatefulMonoBehavior<BallControl.States>
 	            break;
 	        default:
 	            throw new ArgumentOutOfRangeException();
-	    }
-
-	    if (_powerupCounter > PowerupFrames)
-	    {
-            _powerupCounter = 0;
-            ActivePowerup = ObstacleControl.PowerupType.None;
-	    }
-        else if (ActivePowerup != ObstacleControl.PowerupType.None)
-	    {
-	        _powerupCounter++;
 	    }
 	}
 
@@ -148,19 +156,32 @@ public class BallControl : StatefulMonoBehavior<BallControl.States>
     {
         if (State == States.Idle)
         {
-            HandleBounce(transform.position - obs.transform.position);
-            
 			if (obs.State < ObstacleControl.States.OneThird)
             {
                 obs.State = obs.State + 1;
+
+                HandleBounce(transform.position - obs.transform.position, CurrentSpeedClass, false);
             }
             else
             {
-                Destroy(obs.gameObject);
+                if (obs.CurrentPowerupType == ObstacleControl.PowerupType.Faster)
+                {
+                    HandleBounce(transform.position - obs.transform.position, 2, true);
+                }
+                else if (obs.CurrentPowerupType == ObstacleControl.PowerupType.Slower)
+                {
+                    HandleBounce(transform.position - obs.transform.position, 0, true);
+                }
+                else
+                {
+                    HandleBounce(transform.position - obs.transform.position, CurrentSpeedClass + 1, false);
+                }
+
                 State = States.Idle;
-                _powerupCounter = 0;
                 ActivePowerup = obs.CurrentPowerupType;
-            }			
+                
+                Destroy(obs.gameObject);
+            }
         }
     }
 
@@ -180,12 +201,12 @@ public class BallControl : StatefulMonoBehavior<BallControl.States>
                 if (State == States.Pause)
                 {
 					//sweet spot scoring
-                    HandleBounce(wall.Normal);
+                    HandleBounce(wall.Normal, CurrentSpeedClass, false);
                     wall.State = WallControl.States.Idle;
                 }
                 else if (State == States.Idle)
                 {
-                    HandleBounce(wall.Normal);
+                    HandleBounce(wall.Normal, CurrentSpeedClass, false);
                     wall.State = WallControl.States.ShortCooldown;
                 }
                 break;
@@ -199,13 +220,13 @@ public class BallControl : StatefulMonoBehavior<BallControl.States>
             case WallControl.States.StrongReflect:
                 if (State == States.Pause)
                 {
-					//sweet spot scoring
-                    HandleStrongBounce(wall.Normal);
+                    //sweet spot scoring
+                    HandleBounce(wall.Normal, CurrentSpeedClass + 1, true);
                     wall.State = WallControl.States.Idle;
                 }
                 else if (State == States.Idle)
                 {
-                    HandleStrongBounce(wall.Normal);
+                    HandleBounce(wall.Normal, CurrentSpeedClass + 1, true);
                     wall.State = WallControl.States.ShortCooldown;
                 }
                 break;
@@ -214,20 +235,27 @@ public class BallControl : StatefulMonoBehavior<BallControl.States>
         }
     }
 
-    private void HandleBounce(Vector2 normal)
+    private void HandleBounce(Vector2 normal, int newSpeedClass, bool forceNewSpeed)
     {
         // Source: http://stackoverflow.com/questions/573084/how-to-calculate-bounce-angle
         var u = (Vector2.Dot(Velocity, normal) / Vector2.Dot(normal, normal)) * normal;
         var w = Velocity - u;
+
+        if (ActivePowerup == ObstacleControl.PowerupType.Faster || ActivePowerup == ObstacleControl.PowerupType.Slower)
+        {
+            if (forceNewSpeed)
+            {
+                CurrentSpeedClass = Mathf.Clamp(newSpeedClass, 0, SpeedClasses.Length);
+            }
+        }
+        else
+        {
+            CurrentSpeedClass = Mathf.Clamp(newSpeedClass, 0, SpeedClasses.Length);
+        }
 
         //TODO: Add friction?
         Velocity = w - u;
 
         State = States.Bounce;
     }
-
-	private void HandleStrongBounce(Vector2 normal){
-		Speed *= StrongReflectMultiplier;
-		HandleBounce (normal);
-	}
 }
