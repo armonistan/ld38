0b2101721a54c7ec8b34bcb581953cad4cd1bfbb
armonistan <armonistan@gmail.com>
2017-04-23 11:09:35 -0500

    Directional Input (Ball Steering) Added. This version affects Velocity (aka BSing adds to speed)
    Added Max Speed Logic
    Exposed Pause Key to Inspector

--- src/ld38/Assets/Scripts/BallControl.cs
+++ src/ld38/Assets/Scripts/BallControl.cs
@@ -13,9 +13,12 @@ public class BallControl : StatefulMonoBehavior<BallControl.States>
         GameOver
     }
 
+	public float DegAngle;
+	public float StrongReflectMultiplier = 2;
     public float Speed;
-    public float DegAngle;
-
+	public float MaxSpeed = 4;
+	public float BallSteeringMagnitude = 1;
+    
     public int PauseFrames = 5;
 
     public float RadAngle
@@ -24,18 +27,40 @@ public class BallControl : StatefulMonoBehavior<BallControl.States>
         set { DegAngle = value * Mathf.Rad2Deg; }
     }
 
+	public KeyCode LeftBallSteering = KeyCode.LeftArrow;
+	public KeyCode RightBallSteering = KeyCode.RightArrow;
+
+	private float BallSteeringRadian;
+
+	private float PERPENDICULAR_RADIAN = Mathf.PI/2;
+
     public Vector2 Velocity
     {
         get
         {
-            return new Vector2(Mathf.Cos(RadAngle), Mathf.Sin(RadAngle)) * Speed * Time.deltaTime;
-        }
+			return new Vector2 (Mathf.Cos (RadAngle), Mathf.Sin (RadAngle)) * Speed * Time.deltaTime;
+		}
         set
         {
             Speed = value.magnitude / Time.deltaTime;
+			if (Speed > MaxSpeed) {
+				Speed = MaxSpeed;
+			}
             RadAngle = Mathf.Atan2(value.y, value.x);
         }
     }
+
+	public Vector2 BallSteeringVector
+	{
+		get
+		{
+			BallSteeringRadian = (PERPENDICULAR_RADIAN + RadAngle) % (Mathf.PI * 2);
+
+			return new Vector2 (Mathf.Cos (BallSteeringRadian), Mathf.Sin (BallSteeringRadian)) * BallSteeringMagnitude * Time.deltaTime;
+		}
+	}
+
+
 
     // Use this for initialization
 	void Start () {
@@ -51,11 +76,10 @@ public class BallControl : StatefulMonoBehavior<BallControl.States>
 
         switch (State)
 	    {
-	        case States.Idle:
-                GetComponent<Renderer>().material.color = Color.white;
-
-	            gameObject.transform.Translate(Velocity);
-                break;
+		case States.Idle:
+				GetComponent<Renderer> ().material.color = Color.white;
+				UpdateBallPosition ();
+	            break;
 	        case States.Pause:
 	            GetComponent<Renderer>().material.color = Color.yellow;
 
@@ -68,13 +92,13 @@ public class BallControl : StatefulMonoBehavior<BallControl.States>
 	                IncrementCounter();
 	            }
 	            break;
-	        case States.Bounce:
-	            GetComponent<Renderer>().material.color = Color.red;
+			case States.Bounce:
+				GetComponent<Renderer> ().material.color = Color.red;
 
-                gameObject.transform.Translate(Velocity);
+				UpdateBallPosition ();
                 break;
 	        case States.GameOver:
-                SceneManager.LoadScene("TestBed");
+                SceneManager.LoadScene("ArmonTestBed");
 	            break;
 	        default:
 	            throw new ArgumentOutOfRangeException();
@@ -139,6 +163,7 @@ public class BallControl : StatefulMonoBehavior<BallControl.States>
             case WallControl.States.Reflect:
                 if (State == States.Pause)
                 {
+					//sweet spot scoring
                     HandleBounce(wall.Normal);
                     wall.State = WallControl.States.Idle;
                 }
@@ -158,12 +183,13 @@ public class BallControl : StatefulMonoBehavior<BallControl.States>
             case WallControl.States.StrongReflect:
                 if (State == States.Pause)
                 {
-                    HandleBounce(wall.Normal);
+					//sweet spot scoring
+                    HandleStrongBounce(wall.Normal);
                     wall.State = WallControl.States.Idle;
                 }
                 else if (State == States.Idle)
                 {
-                    HandleBounce(wall.Normal);
+                    HandleStrongBounce(wall.Normal);
                     wall.State = WallControl.States.ShortCooldown;
                 }
                 break;
@@ -183,4 +209,19 @@ public class BallControl : StatefulMonoBehavior<BallControl.States>
 
         State = States.Bounce;
     }
+
+	private void HandleStrongBounce(Vector2 normal){
+		Speed *= StrongReflectMultiplier;
+		HandleBounce (normal);
+	}
+
+	private void UpdateBallPosition(){
+		if (Input.GetKey (LeftBallSteering)) {
+			Velocity += BallSteeringVector;
+		} else if (Input.GetKey (RightBallSteering)) {
+			Velocity -= BallSteeringVector;
+		}
+
+		gameObject.transform.Translate(Velocity);
+	}
 }
